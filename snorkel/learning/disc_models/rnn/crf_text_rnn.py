3129bf4cf0f683988c21a2da690c634e152ea3b3
Kyrill Pugatschewski <kyrill_p@gmx.net>
2017-12-12 18:32:51 +0100

    character embeddings

--- snorkel/learning/disc_models/rnn/crf_text_rnn.py
+++ snorkel/learning/disc_models/rnn/crf_text_rnn.py
@@ -24,7 +24,11 @@ class CRFTextRNN(RNNBase):
         if not hasattr(self, 'word_dict'):
             self.word_dict = SymbolTable()
 
-        data, ends, sent_buf = [], [], []
+        if not hasattr(self, 'char_dict'):
+            self.char_dict = SymbolTable()
+
+        max_word_len = 0
+        data, ends, sent_buf, words, word_buf = [], [], [], [], []
         for candidate in candidates:
             tok = candidate.get_contexts()[1].text
             index = candidate.get_contexts()[2].text
@@ -35,7 +39,14 @@ class CRFTextRNN(RNNBase):
                 ends.append(len(sent_buf))
                 sent_buf = []
 
+                c = self.char_dict.get if extend else self.char_dict.lookup
+                sent_words = [np.array(map(c, chars)) for chars in word_buf]
+                words.append(np.array(sent_words))
+                word_buf = []
+
             sent_buf.append(tok)
+            word_buf.append(list(tok))
+            max_word_len = max(max_word_len, len(tok))
 
         marg = []
         if marginals is not None:
@@ -64,18 +75,25 @@ class CRFTextRNN(RNNBase):
                 marg = marg[indexes]
             if dev_labels is not None:
                 aligned_dev_labels = aligned_dev_labels[indexes]
+            if words:
+                words = np.array(words)[indexes]
             print('Shuffled data for LSTM')
 
-        return data, ends, marg, aligned_dev_labels
+        words = words if len(words) > 0 else None
+        return data, ends, marg, aligned_dev_labels, words, max_word_len
 
-    def _build_model(self, dim=50, attn_window=None, max_len=20,
-                     cell_type=tf.contrib.rnn.BasicLSTMCell,
-                     word_dict=SymbolTable(), **kwargs):
+    def _build_model(self, dim=50, dim_char=50, attn_window=None, max_len=20,
+                     cell_type=tf.contrib.rnn.BasicLSTMCell, max_word_len=10,
+                     word_dict=SymbolTable(), char_dict=SymbolTable(), **kwargs):
 
         # Set the word dictionary passed in as the word_dict for the instance
         self.max_len = max_len
         self.word_dict = word_dict
         vocab_size = word_dict.len()
+
+        self.max_word_len = max_word_len
+        self.char_dict = char_dict
+        n_chars = char_dict.len()
 
         # Define input layers
         self.sentences = tf.placeholder(tf.int32, [None, None])
@@ -90,6 +108,39 @@ class CRFTextRNN(RNNBase):
             tf.random_normal((vocab_size - 1, dim), stddev=SD, seed=s1))
         embedding = tf.concat([tf.zeros([1, dim]), emb_var], axis=0)
         inputs = tf.nn.embedding_lookup(embedding, self.sentences)
+
+        # Character embedding
+        # shape = (batch_size, max_sent_len, max_word_len)
+        self.words = tf.placeholder(tf.int32, [None, None, None])
+        self.word_lengths = tf.placeholder(tf.int32, shape=[None, None])
+
+        char_var = tf.get_variable(name='char_embeddings',
+                                   dtype=tf.float32, shape=[n_chars, dim_char])
+        char_embedding = tf.nn.embedding_lookup(char_var, self.words)
+
+        char_s = tf.shape(char_embedding)
+        # shape = (batch x sentence, word, dim of char embeddings)
+        char_embedding = tf.reshape(char_embedding, shape=[
+                                    char_s[0] * char_s[1], char_s[-2], dim_char])
+        word_lengths = tf.reshape(self.word_lengths, shape=[char_s[0] * char_s[1]])
+
+        init = tf.contrib.layers.xavier_initializer(seed=s2)
+        with tf.variable_scope(self.name + '_char', reuse=False, initializer=init):
+            char_fw_cell = cell_type(dim_char, state_is_tuple=True)
+            char_bw_cell = cell_type(dim_char, state_is_tuple=True)
+
+            _, ((_, char_fw_out), (_, char_bw_out)) = tf.nn.bidirectional_dynamic_rnn(
+                char_fw_cell, char_bw_cell, char_embedding,
+                sequence_length=word_lengths,
+                dtype=tf.float32
+            )
+        char_out = tf.concat([char_fw_out, char_bw_out], axis=-1)
+        char_rep = tf.reshape(char_out, shape=[-1, char_s[1], 2 * dim_char])
+        inputs = tf.concat([inputs, char_rep], axis=-1)
+
+        # Add dropout layer
+        self.keep_prob = tf.placeholder(tf.float32)
+        inputs_dropout = tf.nn.dropout(inputs, self.keep_prob, seed=s3)
 
         # Build RNN graph
         batch_size = tf.shape(self.sentences)[0]
@@ -109,8 +160,15 @@ class CRFTextRNN(RNNBase):
             # Construct RNN
             initial_state_fw = fw_cell.zero_state(batch_size, tf.float32)
             initial_state_bw = bw_cell.zero_state(batch_size, tf.float32)
+            # rnn_out, _ = tf.nn.bidirectional_dynamic_rnn(
+            #     fw_cell, bw_cell, inputs,
+            #     sequence_length=self.sentence_lengths,
+            #     initial_state_fw=initial_state_fw,
+            #     initial_state_bw=initial_state_bw,
+            #     time_major=False
+            # )
             rnn_out, _ = tf.nn.bidirectional_dynamic_rnn(
-                fw_cell, bw_cell, inputs,
+                fw_cell, bw_cell, inputs_dropout,
                 sequence_length=self.sentence_lengths,
                 initial_state_fw=initial_state_fw,
                 initial_state_bw=initial_state_bw,
@@ -121,19 +179,20 @@ class CRFTextRNN(RNNBase):
             rnn_out, dim, self.sentence_lengths)
 
         # Add dropout layer
-        self.keep_prob = tf.placeholder(tf.float32)
-        potentials_dropout = tf.nn.dropout(potentials, self.keep_prob, seed=s3)
+        # self.keep_prob = tf.placeholder(tf.float32)
+        # potentials_dropout = tf.nn.dropout(potentials, self.keep_prob, seed=s3)
 
         # Build activation layer
         # self.Y = tf.placeholder(tf.float32, [None, self.cardinality])
         self.Y = tf.placeholder(tf.float32, [None, None, self.cardinality])
-        self.train_labels = tf.placeholder(tf.int32, [None, self.cardinality])
+        # self.train_labels = tf.placeholder(tf.int32, [None, self.cardinality])
+        self.train_labels = tf.placeholder(tf.int32, [None, self.max_len])
 
         W = tf.Variable(tf.random_normal((2 * dim, self.cardinality),
                                          stddev=SD, seed=s4))
         b = tf.Variable(np.zeros(self.cardinality), dtype=tf.float32)
-        # self.logits = tf.matmul(potentials, W) + b
-        self.logits = tf.matmul(potentials_dropout, W) + b
+        self.logits = tf.matmul(potentials, W) + b
+        # self.logits = tf.matmul(potentials_dropout, W) + b
         self.logits = tf.reshape(
             self.logits, [-1, ntime_steps, self.cardinality])
         # self.marginals_op = tf.nn.softmax(self.logits)
@@ -154,11 +213,11 @@ class CRFTextRNN(RNNBase):
         # self.pred, viterbi_score = tf.contrib.crf.viterbi_decode(
         #     self.logits, self.transition_params)
 
-        losses = tf.nn.softmax_cross_entropy_with_logits(
-            logits=self.logits, labels=self.Y)
+        # losses = tf.nn.softmax_cross_entropy_with_logits(
+        #     logits=self.logits, labels=self.Y)
 
-        # losses = tf.nn.sparse_softmax_cross_entropy_with_logits(
-        #     logits=self.logits, labels=self.train_labels)
+        losses = tf.nn.sparse_softmax_cross_entropy_with_logits(
+            logits=self.logits, labels=self.train_labels)
 
         mask = tf.sequence_mask(self.sentence_lengths)
         losses = tf.boolean_mask(losses, mask)
@@ -169,24 +228,22 @@ class CRFTextRNN(RNNBase):
         self.lr = tf.placeholder(tf.float32)
         self.optimizer = tf.train.AdamOptimizer(self.lr).minimize(self.loss)
 
-    def _construct_feed_dict(self, X_b, Y_b, lr=0.01, dropout=None, train_labels=None, **kwargs):
-        X_b, len_b, Y_b, L_b = self._make_tensor(X_b, Y_b, train_labels)
-        # print()
-        # print(X_b)
-        # print()
-        # print(len_b)
-        # print()
-        # print(Y_b)
+    def _construct_feed_dict(self, X_b, Y_b, lr=0.01, dropout=None, train_labels=None,
+                             chars=None, **kwargs):
+        X_b, len_b, Y_b, L_b, C_b, len_c = self._make_tensor(X_b, Y_b, train_labels, chars)
+
         return {
             self.sentences:        X_b,
             self.sentence_lengths: len_b,
             self.Y:                Y_b,
             self.keep_prob:        dropout or 1.0,
             self.lr:               lr,
-            self.train_labels:     L_b
+            self.train_labels:     L_b,
+            self.words:            C_b,
+            self.word_lengths:     len_c
         }
 
-    def _make_tensor(self, x, y=None, z=None):
+    def _make_tensor(self, x, y=None, z=None, c=None):
         """Construct input tensor with padding
             Builds a matrix of symbols corresponding to @self.word_dict for the
             current batch and an array of true sentence lengths
@@ -195,9 +252,37 @@ class CRFTextRNN(RNNBase):
         x_batch = np.zeros((batch_size, self.max_len), dtype=np.int32)
         y_batch = np.zeros((batch_size, self.max_len, self.cardinality))
         z_batch = np.zeros((batch_size, self.max_len), dtype=np.int32)
+        c_batch = np.zeros((batch_size, self.max_len, self.max_word_len), dtype=np.int32)
         len_batch = np.zeros(batch_size, dtype=np.int32)
+        len_words = np.zeros((batch_size, self.max_len), dtype=np.int32)
 
-        if z is not None:
+        if c is not None and y is None and z is None:
+            for j, (token_ids, words) in enumerate(zip(x, c)):
+                t = min(len(token_ids), self.max_len)
+                x_batch[j, 0:t] = token_ids[0:t]
+                len_batch[j] = t
+
+                for x, y in enumerate(words[0:t]):
+                    c_batch[j][x][0:len(y)] = y
+
+                char_t = np.array([min(len(word_ids), self.max_word_len) for word_ids in words])
+                len_words[j][0:len(char_t)] = char_t
+
+        elif c is not None:
+            for j, (token_ids, marginals, labels, words) in enumerate(zip(x, y, z, c)):
+                t = min(len(token_ids), self.max_len)
+                x_batch[j, 0:t] = token_ids[0:t]
+                y_batch[j, 0:t] = marginals[0:t]
+                z_batch[j, 0:t] = labels[0:t]
+                len_batch[j] = t
+
+                for x, y in enumerate(words[0:t]):
+                    c_batch[j][x][0:len(y)] = y
+
+                char_t = np.array([min(len(word_ids), self.max_word_len) for word_ids in words])
+                len_words[j][0:len(char_t)] = char_t
+
+        elif z is not None:
             for j, (token_ids, marginals, labels) in enumerate(zip(x, y, z)):
                 t = min(len(token_ids), self.max_len)
                 x_batch[j, 0:t] = token_ids[0:t]
@@ -218,22 +303,24 @@ class CRFTextRNN(RNNBase):
                 x_batch[j, 0:t] = token_ids[0:t]
                 len_batch[j] = t
 
-        return x_batch, len_batch, y_batch, z_batch
+        return x_batch, len_batch, y_batch, z_batch, c_batch, len_words
 
-    def predictions(self, X, b=0.5, batch_size=None):
+    def predictions(self, X, b=0.5, batch_size=None, words=None):
 
         if isinstance(X[0], Candidate):
-            X_test, ends, _, _ = self._preprocess_data(X, extend=False)
+            X_test, ends, _, _, words, _ = self._preprocess_data(X, extend=False)
             self._check_max_sentence_length(ends)
         else:
             X_test = X
 
         # Make tensor and run prediction op
-        x, x_len, _, _ = self._make_tensor(X_test)
+        x, x_len, _, _, _words, _words_len = self._make_tensor(X_test, c=words)
         pred = self.session.run(self.pred, {
             self.sentences:        x,
             self.sentence_lengths: x_len,
             self.keep_prob:        1.0,
+            self.words:            _words,
+            self.word_lengths:     _words_len
         })
 
         # logit_scores = self.session.run(self.logits, {
@@ -264,9 +351,9 @@ class CRFTextRNN(RNNBase):
         # correct = np.where([predictions == Y_test])[0].shape[0]
         # return correct / float(Y_test.shape[0])
 
-        X_test, ends, _, _ = self._preprocess_data(X_test, extend=False)
+        X_test, ends, _, _, words, _ = self._preprocess_data(X_test, extend=False)
         self._check_max_sentence_length(ends)
-        predictions = self.predictions(X_test, b=b, batch_size=batch_size)
+        predictions = self.predictions(X_test, b=b, batch_size=batch_size, words=words)
 
         # # Convert Y_test to dense numpy array
         # try:
@@ -282,10 +369,7 @@ class CRFTextRNN(RNNBase):
             cand_idx = end_idx
         Y_test = np.array(labels)
 
-        # print(X_test)
-        # print(Y_test)
-        # print(predictions)
-
+        correct = 0
         # correct = np.where([predictions == Y_test])[0].shape[0]
         # return correct / float(Y_test.shape[0])
 
@@ -299,6 +383,10 @@ class CRFTextRNN(RNNBase):
 
             for tag_pred, tag_gold in zip(sent_pred, sent_gold):
                 token_num += 1
+
+                if tag_pred == tag_gold:
+                    correct += 1
+
                 if tag_pred != tag_gold:
                     pred_err += 1
 
@@ -325,32 +413,35 @@ class CRFTextRNN(RNNBase):
         if pred_other_num == 0:
             pred_other_num = 1
 
-        return float(token_err) / token_num, float(sent_err) / sent_num, \
+        return float(correct) / token_num, \
+            float(token_err) / token_num, float(sent_err) / sent_num, \
             float(gold_other_err) / gold_other_num, float(pred_other_err) / pred_other_num
 
-    def train(self, X_train, Y_train, dev_labels=None, X_dev=None, max_sentence_length=None, shuffle=True,
-              **kwargs):
+    def train(self, X_train, Y_train, dev_labels=None, X_dev=None, max_sentence_length=None,
+              shuffle=True, max_word_length=None, **kwargs):
         """
         Perform preprocessing of data, construct dataset-specific model, then
         train.
         """
         # Text preprocessing
-        X_train, ends, Y_train, train_labels = self._preprocess_data(
+        X_train, ends, Y_train, train_labels, train_words, max_word_len = self._preprocess_data(
             X_train, Y_train, dev_labels=dev_labels, extend=True, shuffle_data=shuffle)
         if X_dev is not None:
-            X_dev, _, _, _ = self._preprocess_data(X_dev, [], extend=False)
+            X_dev, _, _, _, _, _ = self._preprocess_data(X_dev, [], extend=False)
 
         # Get max sentence size
         max_len = max_sentence_length or max(len(x) for x in X_train)
         self._check_max_sentence_length(ends, max_len=max_len)
+        max_word_len = max_word_length or max_word_len
 
         # Train model- note we pass word_dict through here so it gets saved...
         # super(RNNBase, self).train(X_train, Y_train, X_dev=X_dev,
         #                            word_dict=self.word_dict, max_len=max_len, train_labels=train_labels, **kwargs)
-        self._train(X_train, Y_train, X_dev=X_dev,
-                    word_dict=self.word_dict, max_len=max_len, dev_labels=train_labels, **kwargs)
+        self._train(X_train, Y_train, X_dev=X_dev, words=train_words, char_dict=self.char_dict,
+                    word_dict=self.word_dict, max_len=max_len, dev_labels=train_labels,
+                    max_word_len=max_word_len, **kwargs)
 
-    def _train(self, X_train, Y_train, dev_labels=None, n_epochs=25, lr=0.01, batch_size=256,
+    def _train(self, X_train, Y_train, dev_labels=None, words=None, n_epochs=25, lr=0.01, batch_size=256,
                rebalance=False, X_dev=None, Y_dev=None, print_freq=5, dev_ckpt=True,
                dev_ckpt_delay=0.75, save_dir='checkpoints', **kwargs):
         """
@@ -433,10 +524,16 @@ class CRFTextRNN(RNNBase):
                 else:
                     batch_labels = None
 
+                if words is not None:
+                    batch_words = words[i:min(n, i + batch_size)]
+                else:
+                    batch_words = None
+
                 feed_dict = self._construct_feed_dict(
                     X_train[i:min(n, i + batch_size)],
                     Y_train[i:min(n, i + batch_size)],
                     train_labels=batch_labels,
+                    chars=batch_words,
                     lr=lr,
                     **kwargs
                 )
